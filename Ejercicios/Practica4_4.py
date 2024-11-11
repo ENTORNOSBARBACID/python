22e57027d3a8b62a9f4de029513d2e889f7281da
BARBACID <39006181+ENTORNOSBARBACID@users.noreply.github.com>
2024-11-11 12:18:32 +0100

    Delete Ejercicios/Practica4_4.py

--- Ejercicios/Practica4_4.py
+++
@@ -1,8 +0,0 @@
-lista=["Alcaeda","C", "Benifasio"]
-listOrder=list(lista)
-listOrder.sort()
-print(listOrder)
-if lista==listOrder:
-    print("The Array is order")
-else:
-    print("Silly, the Array is not order")
