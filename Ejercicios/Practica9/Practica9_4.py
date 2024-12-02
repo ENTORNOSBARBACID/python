0fd7306554a00cb0a7088850c1b783935c17710c
BARBACID <39006181+ENTORNOSBARBACID@users.noreply.github.com>
2024-12-02 13:46:01 +0100

    Add files via upload

--- Ejercicios/Practica9/Practica9_4.py
+++ Ejercicios/Practica9/Practica9_4.py
@@ -1,5 +1,16 @@
 with open("Ejercicios/Practica9/text/texto.txt") as f:
     palabra=f.readline().split(" ")
     
+cleanPalabra=[]
+
 for i in palabra:
-    print(i)
+    if i not in cleanPalabra:
+        cleanPalabra.append(i)
+
+mas=""
+
+for i in cleanPalabra:
+    if palabra.count(i)>palabra.count(mas):
+        mas=i
+    
+print("La palabra que mas sale es "+mas+", apareciendo un total de "+str(palabra.count(mas))+" veces")
